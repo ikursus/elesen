c5db1aabebd7b6072c10c1c14a133ac9b4420a87
Amirol Zolkifli <usahaniaga.com@gmail.com>
2018-08-27 14:46:09 +0800

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,9 @@
 <?php
-
-
-Route::group(['middleware' => 'auth', 'prefix' => 'panel'], function() {
-
-    Route::get('user', function () {
-        return 'Halaman user';
-    });
-
-    Route::get('admin', function () {
-        return 'Halaman admin';
-    });
-
-});
-
-Route::get('/', function () {
-    # Jika tidak, paparkan template welcome
-    return view('welcome');
-})->where('username', '[A-Za-z]+');
-
+# Route untuk authentication (login/logout/register/password reset)
 Auth::routes();
-
+# Route halaman selepas login
 Route::get('/home', 'HomeController@index')->name('home');
+# Route halaman utama aplikasi
+Route::get('/', function () {
+    return view('welcome');
+});
