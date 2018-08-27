34d5f6bc800711680c7b0efe6d567eaee91930b0
Amirol Zolkifli <usahaniaga.com@gmail.com>
2018-08-27 14:51:39 +0800

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', function () {
     return view('welcome');
 });
+
+# Route untuk paparan senarai users
+Route::get('/users', function () {
+    return 'Halaman Senarai Users';
+});
+
+# Route untuk tambah user baru
+Route::get('/users/add', function () {
+    return 'Halaman Tambah User Baru';
+});
+
+# Route untuk edit user
+Route::get('/users/{id}/edit', function () {
+    return 'Halaman Tambah User Baru';
+});
