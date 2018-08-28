db076f870f1a5d6b16f9cf1c45c125ee0b2cc569
Amirol Zolkifli <usahaniaga.com@gmail.com>
2018-08-28 09:58:06 +0800

    Update categories controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ Route::get('/users/add', 'UsersController@create')->name('users.create');
 
 # Route untuk edit user
 Route::get('/users/{id}/edit', 'UsersController@edit')->name('users.edit');
+
+# Paparkan senarai categories
+Route::get('/categories', 'CategoriesController@index');
+# Paparkan borang tambah category baru
+Route::get('/categories/add', 'CategoriesController@create');
+# Simpan data tambah category baru
+Route::post('/categories/add', 'CategoriesController@store');
+# Paparkan borang edit category
+Route::get('/categories/{id}/edit', 'CategoriesController@edit');
+# Simpan data kemaskini category
+Route::patch('/categories/{id}/edit', 'CategoriesController@update');
+# Delete data berdasarkan rekod id
+Route::delete('/categories/{id}', 'CategoriesController@destroy');
