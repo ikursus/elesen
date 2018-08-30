46ea9c77bb4d7d6281c5cf2b316303b42c163875
Amirol Zolkifli <usahaniaga.com@gmail.com>
2018-08-30 10:34:11 +0800

    Add action column to datatables users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,7 +26,49 @@ class UsersController extends Controller
         # Query rekod users dari DB
         $query = User::select('id', 'nama', 'email', 'ic', 'role');
         # Beri response datatables beserta query
-        return DataTables::of($query)->make(true);
+        return DataTables::of($query)
+        ->addColumn('action', function ($item) {
+            return '
+
+<a class="btn btn-sm btn-info" href="' . route('users.edit', ['id' => $item->id ]) . '">EDIT</a>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#modal-delete-'. $item->id .'">
+	DELETE
+</button>
+
+<!-- Modal -->
+<form method="POST" action="'. route('users.destroy', ['id' => $item->id ]) .'">
+' . csrf_field() . '
+<input type="hidden" name="_method" value="delete">
+
+<div class="modal fade" id="modal-delete-'. $item->id .'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+	<div class="modal-content">
+	  <div class="modal-header">
+		<h5 class="modal-title" id="exampleModalLabel">Pengesahan</h5>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		  <span aria-hidden="true">&times;</span>
+		</button>
+	  </div>
+	  <div class="modal-body">
+		Adakah anda bersetuju untuk menghapuskan data berikut:
+
+		<p>Nama: '. $item->nama .'</p>
+	  </div>
+	  <div class="modal-footer">
+		<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+		<button type="submit" class="btn btn-danger">Confirm</button>
+	  </div>
+	</div>
+  </div>
+</div>
+
+</form>
+
+            ';
+        })
+        ->make(true);
     }
 
     # Papar borang tambah user
